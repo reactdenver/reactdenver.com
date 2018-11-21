370c3d87da8597adc71c462f6417f2624a12ad2d
Sheelah Brennan <slb53@hotmail.com>
2018-11-21 11:04:24 -0700

    Add links to favicons via Helmet
    We could use a Gatsby plugin for this but I don't think that's
    worth doing since this site isn't on Gatsby v2 yet (and
    all popular plugins are). See:
    https://github.com/gatsbyjs/gatsby/issues/2987#issuecomment-404972215

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,10 +5,20 @@ import Helmet from 'react-helmet';
 import Navbar from '../components/Navbar';
 import './all.sass';
 import logo from '../img/reactdenverlogo.png';
+import largeFavicon from '../img/favicon-32x32.png';
+import smallFavicon from '../img/favicon-16x16.png';
+import appleTouchIcon from '../img/apple-touch-icon.png';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="React Denver Meetup" />
+    <Helmet
+      title="React Denver Meetup"
+      link={[
+        { rel: 'icon', type: 'image/png', href: `${largeFavicon}` },
+        { rel: 'icon', type: 'image/png', href: `${smallFavicon}` },
+        { rel: 'apple-touch-icon', type: 'image/png', href: `${appleTouchIcon}` }
+      ]}
+    />
     <Navbar />
     <div>
       <center>
