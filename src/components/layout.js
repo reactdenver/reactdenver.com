21f20a53d02f5698a3e93c7ab01f06419753afe3
Kevin McKernan <kevin@mckernan.in>
2019-03-12 20:07:01 -0600

    update duplicate header in <Layout />

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
 
 import styled from "styled-components";
@@ -32,15 +31,6 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <GlobalStyle />
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
         <Header
           siteTitle={data.site.siteMetadata.title}
           menuLinks={data.site.siteMetadata.menuLinks}
