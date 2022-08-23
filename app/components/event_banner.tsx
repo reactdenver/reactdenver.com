2237b7b40a0d5309fc3de2fca7ca7302e83cea06
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-23 13:37:29 -0600

    next event banner created

--- app/components/event_banner.tsx
+++ app/components/event_banner.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+
+//need to load next event data and have banner link to single event page (slug)
+
 export default function EventBanner() {
-  return(
-    <div>
+  const banner_style = {
+    color: 'white',
+    backgroundColor: '#bd7ff3',
+    padding: "10px",
+    textAlign: 'center'
+  }
+  return(  
+    <div style={banner_style}>
       Next meetup: Thurs, Sept 04
     </div>
   )
-}
+};
+
