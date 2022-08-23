1600b37e113971402c71be11ac7e6af1751b6211
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-23 12:49:56 -0600

    Create $slug.tsx

---
+++ app/routes/events/$slug.tsx
@@ -0,0 +1,12 @@
+import { marked } from "marked";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export default function EventSlug() {
+  return (
+    <main>
+      single post
+    </main>
+  )
+}
