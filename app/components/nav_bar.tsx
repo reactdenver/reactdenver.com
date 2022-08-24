966479a96cc4350a24ca79aff7b7e8564b808295
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-24 10:04:41 -0600

    nav bar styling close

--- app/components/nav_bar.tsx
+++ app/components/nav_bar.tsx
@@ -1,73 +1,74 @@
-import React from 'react';
-import { Link } from '@remix-run/react';
-import ReactDenverLogo from '../assets/react_denver_logo.svg';
-import { NodeOnDiskFile } from '@remix-run/node';
+import React from "react";
+import { Link } from "@remix-run/react";
+import ReactDenverLogo from "../assets/react_denver_logo.svg";
 
 export default function NavBar() {
-  const navbar_style = {
-    borderBottom: '#DDDDDD 1px solid',
-    height: '60px',
-    paddingLeft: '5%',
-    paddingRight: '5%',
-    lineHeight: 'height'
-  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        borderBottom: "1px solid #DDDDDD",
+        maxHeight: "60px",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ display: "flex", flex: "0 1 30%", paddingLeft: "5%" }}>
+        <Link style={{ display: "flex" }} to={"/"}>
+          <img
+            style={{ maxHeight: "50px", paddingTop: "15px" }}
+            src={ReactDenverLogo}
+          />
+        </Link>
+        <Link style={{ color: "black", textDecoration: "none" }} to={"/"}>
+          <h1
+            style={{
+              fontSize: "1.8em",
+              fontWeight: "400",
+              paddingLeft: "60px",
+              textDecoration: "none",
+            }}
+          >
+            React Denver
+          </h1>
+        </Link>
+      </div>
 
-  const title_style = {
-    fontSize: '1.8em',
-    fontWeight: '400',
-    color: 'black'
-  };
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          flex: "1",
+          paddingRight: "5%",
+        }}
+      >
+        <Link
+          style={{
+            padding: "0 10px 0 5px",
+            color: "#666666",
+            textDecoration: "none",
+          }}
+          to="/events"
+        >
+          Events
+        </Link>
+        <Link
+          style={{
+            padding: "0 5px 0 10px",
+            color: "#666666",
+            textDecoration: "none",
+          }}
+          to="/talk"
+        >
+          Submit a talk
+        </Link>
+      </div>
 
-  const list_style = {
-    
-    li_left: {
-      display: 'inline',
-      float: 'left',
-    },
-    
-    li_right: {
-      display: 'inline',
-      float: 'right',
-      alightItems: 'center'
-    },
-    ul: {
-      display: 'flex',
-      listStyleType: 'none',
-      margin:0,
-      padding: 0,
-      alignItems: 'center'
-    },
-    link: {
-      display: 'block',
-      width: '100px',
-      textAlign: 'center',
-      textDecoration: 'none',
-      color: '#666666',
-    },
-    link_title: {
-      width: '200px',
-      textDecoration: 'none'
-    }
-  }
-
-  return(
-    <div style={navbar_style} >
-      <ul style={list_style.ul}>
-        <li style={list_style.li_left}><Link style={list_style.link} to='/'><img style={{maxHeight: '50px'}} src={ReactDenverLogo}/></Link></li>
-        <li style={list_style.li_left}><Link style={list_style.link, list_style.link_title} to='/'><div style={title_style}>React Denver</div></Link> </li>
-        <li style={list_style.li_right}><Link style={list_style.link} to='/events'>Events</Link></li>
-        <li style={list_style.li_right}><Link style={list_style.link} to='/talk'>Submit a talk</Link></li>
-      </ul>
-      
-      
-
-        {/* <Link to="/faq">
+      {/* <Link to="/faq">
           FAQ
         </Link>
         <Link to="/code_of_conduct">
           Code of Conduct
-        </Link> */} 
-      
+        </Link> */}
     </div>
-  )
+  );
 }
