b21d74b821839c35d322c3e3bedb52240497de10
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-24 10:57:57 -0600

    added link icons

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,11 @@
 import { Link } from "@remix-run/react";
-import ReactDenverLogo from "../assets/react_denver_logo.svg";
 import PrivacyPolicy from "~/routes/privacy_policy";
+
+import ReactDenverLogo from "../assets/react_denver_logo.svg";
+import YouTube from "../assets/YouTube.svg";
+import Discord from "../assets/Discord.svg";
+import GitHub from "../assets/GitHub.svg";
+import Twitter from "../assets/Twitter.svg";
 
 export default function Footer() {
   return (
@@ -14,11 +19,19 @@ export default function Footer() {
       }}
     >
       <img style={{ maxHeight: "50px" }} src={ReactDenverLogo} />
-      <a href="">discord</a>
-      <a href="">github</a>
-      <a href="">twitter</a>
-      <a href="">youtube</a>
-      <a href="">netlify</a>
+      <a href="">
+        <img style={{ maxHeight: "50px" }} src={Discord} />
+      </a>
+      <a href="https://github.com/reactdenver">
+        <img style={{ maxHeight: "50px" }} src={GitHub} />
+      </a>
+      <a href="https://mobile.twitter.com/ReactDenver">
+        <img style={{ maxHeight: "50px" }} src={Twitter} />
+      </a>
+      <a href="https://www.youtube.com/c/ReactDenver">
+        <img style={{ maxHeight: "50px" }} src={YouTube} />
+      </a>
+      <a href="">Netlify</a>
       <Link
         style={{
           padding: "0 5px 0 10px",
