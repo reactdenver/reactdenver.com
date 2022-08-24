08c1b32fefce5fc29ad4a5d80a39680de8f7b28d
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-24 10:47:46 -0600

    added privacy policy

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@remix-run/react";
 import ReactDenverLogo from "../assets/react_denver_logo.svg";
+import PrivacyPolicy from "~/routes/privacy_policy";
 
 export default function Footer() {
   return (
@@ -13,7 +14,21 @@ export default function Footer() {
       }}
     >
       <img style={{ maxHeight: "50px" }} src={ReactDenverLogo} />
-
+      <a href="">discord</a>
+      <a href="">github</a>
+      <a href="">twitter</a>
+      <a href="">youtube</a>
+      <a href="">netlify</a>
+      <Link
+        style={{
+          padding: "0 5px 0 10px",
+          color: "#666666",
+          textDecoration: "none",
+        }}
+        to="/privacy_policy"
+      >
+        Privacy Policy
+      </Link>
       <Link
         style={{
           padding: "0 5px 0 10px",
---
+++ app/routes/privacy_policy.tsx
@@ -0,0 +1,12 @@
+import Footer from "~/components/footer";
+import NavBar from "~/components/nav_bar";
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <NavBar />
+      Privacy Policy goes here
+      <Footer />
+    </div>
+  );
+}
