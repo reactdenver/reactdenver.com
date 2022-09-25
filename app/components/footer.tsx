dc0e953e6846866c294140fc4faaeb405d0c2c2f
Pete Fowler <fowler.pete@gmail.com>
2022-09-25 14:37:45 -0600

    Add proper link / routing
    Need style for code of conduct and privacy policy

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "~/style/index.css";
 
+import { Link } from "@remix-run/react";
+
+
 export default function Footer() {
 
   return <div id='footer-box'>
@@ -9,8 +12,8 @@ export default function Footer() {
     <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} alt="React Denver icon"/>
     <img id="footer_skyline" src={require("../assets/footer_skyline.png")} alt="City skyline"/>
     <div id='footer-links'>
-      <a href="#" >Code of Conduct</a>
-      <a href="#" >Privacy Policy</a>
+      <Link to="/code_of_conduct">Code of Conduct</Link>
+      <Link to="/privacy_policy">Privacy Policy</Link>
     </div>
   </div>
 }
