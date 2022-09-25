e35ad115fd066a8831d2b056faf88b41d7ec0662
Pete Fowler <fowler.pete@gmail.com>
2022-09-25 15:14:59 -0600

    Add link home on RD footer logo

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -9,7 +9,9 @@ export default function Footer() {
   return <div id='footer-box'>
     <div id='footer'>
     </div>
-    <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} alt="React Denver icon"/>
+    <Link to="/">
+      <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} alt="React Denver icon"/>
+    </Link>
     <img id="footer_skyline" src={require("../assets/footer_skyline.png")} alt="City skyline"/>
     <div id='footer-links'>
       <Link to="/code_of_conduct">Code of Conduct</Link>
