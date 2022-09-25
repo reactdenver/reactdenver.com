8292d729b87ec34061526ff51e5499925f1c7daf
Pete Fowler <fowler.pete@gmail.com>
2022-09-25 14:04:38 -0600

    Add footer, position RD logo

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "~/style/index.css";
+
+export default function Footer() {
+
+  return <div id='footer-box'>
+    <div id='footer'>
+    </div>
+    <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} />
+  </div>
+}
