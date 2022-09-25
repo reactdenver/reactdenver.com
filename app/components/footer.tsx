9d90014e42efb6756b63a3b6eb449e23023e75b9
Pete Fowler <fowler.pete@gmail.com>
2022-09-25 14:26:07 -0600

    Add and position links. Need routes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,7 +6,11 @@ export default function Footer() {
   return <div id='footer-box'>
     <div id='footer'>
     </div>
-    <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} />
-    <img id="footer_skyline" src={require("../assets/footer_skyline.png")} />
+    <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} alt="React Denver icon"/>
+    <img id="footer_skyline" src={require("../assets/footer_skyline.png")} alt="City skyline"/>
+    <div id='footer-links'>
+      <a href="#" >Code of Conduct</a>
+      <a href="#" >Privacy Policy</a>
+    </div>
   </div>
 }
