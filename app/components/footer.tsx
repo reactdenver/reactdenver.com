f9a45fa78fc16fdd172edd0f00d259afa50472ac
Pete Fowler <fowler.pete@gmail.com>
2022-09-25 14:11:50 -0600

    Add and position footer skyline

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,5 +7,6 @@ export default function Footer() {
     <div id='footer'>
     </div>
     <img id="RD_footer_icon" src={require("../assets/RD_footer_icon.png")} />
+    <img id="footer_skyline" src={require("../assets/footer_skyline.png")} />
   </div>
 }
