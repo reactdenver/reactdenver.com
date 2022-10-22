6603348c8b63109d7f609884319ee7e7027488e2
Pete Fowler <fowler.pete@gmail.com>
2022-10-21 19:47:50 -0600

    Remove component and continue in events route

--- app/routes/events/index.tsx
+++ app/routes/events/index.tsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import { getEventsJson } from "~/utils/events.server";
+import PastEvent from "~/components/past_event";
 
 export async function loader() {
   const events = await getEventsJson();
@@ -17,17 +18,28 @@ export default function Events() {
 
   console.log(events);
   return (
-    <div>
-      <main>
-        <h1>Events list</h1>
-        <ul>
-          {events.map((event) => (
-            <li>
-              <Link to={`./${event.slug}`}>{event.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </main>
+  <div className={'past-event'}>
+    <h4 className={'past-event__headline'}>Previous Events:</h4>
+    <img className={'past-event__image'} src='' alt=''></img>
+    <div className={'past-event__content'}>
+      <p className={'past-event__text_small'}>date</p>
+      <h3 className={'past-event__text_large'}>description</h3>
+      <p className={'past-event__text_small'}>speakers</p>
+      <p className={'past-event__text_small'}>location</p>
     </div>
+  </div>
+    // <div>
+    //   <main>
+    //     <h1>Events list</h1>
+    //     <PastEvent />
+    //     <ul>
+    //       {events.map((event) => (
+    //         <li>
+    //           <Link to={`./${event.slug}`}>{event.title}</Link>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </main>
+    // </div>
   );
 }
