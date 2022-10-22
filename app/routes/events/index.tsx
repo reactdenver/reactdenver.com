39ba81e2165543a8bf02004065e3b7e6fa07d6c2
Pete Fowler <fowler.pete@gmail.com>
2022-10-22 08:03:16 -0600

    Format date string with date-fns

--- app/routes/events/index.tsx
+++ app/routes/events/index.tsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import { getEventsJson } from "~/utils/events.server";
+import { format } from 'date-fns';
 
 export async function loader() {
   const events = await getEventsJson();
@@ -18,16 +19,18 @@ export default function Events() {
   // Will need to add a filter to show past events and one future event
 
   console.log(events);
+
+  // console.log(format(new Date(events[0].date), 'EEE, MMM d, y'));
   
   return (
   <div className={'past-events'}>
     <h4 className={'past-events__headline'}>Previous Events:</h4>
     {events.map(event => 
     <Link key={event.id} className={'past-event__content'} to={`./${event.slug}`}>
-      <img className={'past-event__image'} src='https://picsum.photos/400/300' alt=''></img>
-      {/*=============== Image source to be changed when available from mdx========= */}
+      <img className={'past-event__image'} src={'https://source.unsplash.com/random/400x300'} alt={event.title}></img>
+      {/*=============== Image source to be changed from placeholder when available========= */}
       <div className={'past-event__text-box'}>
-        <p className={'past-event__text_small'}>{new Date(event.date).toDateString()}</p>
+        <p className={'past-event__text_small'}>{format(new Date(event.date), 'EEE, MMM d, y')}</p>
         <h3 className={'past-event__text_large'}>{event.title}</h3>
         <p className={'past-event__text_small'}>{event.speakers?.map(speaker => `${speaker.name}`)}</p>
         <p className={'past-event__text_small'}>{event.location}</p>
