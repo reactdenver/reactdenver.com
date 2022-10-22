4afc952eb63ada0415b946cd665c4dfbc86bdec9
Pete Fowler <fowler.pete@gmail.com>
2022-10-21 19:22:35 -0600

    Add past event HTML

--- app/components/past_event/index.tsx
+++ app/components/past_event/index.tsx
@@ -4,7 +4,15 @@ export const links = () => [{ rel: "stylesheet", href: styles }];
 
 export default function PastEvent({ event }) {
 
-  return <div className={}>
-
+  return (
+  <div className={'past-event'}>
+    <img className={'past-event__image'} src='' alt=''></img>
+    <div className={'past-event__content'}>
+      <p className={'past-event__text_small'}></p>
+      <h3 className={'past-event__text_large'}></h3>
+      <p className={'past-event__text_small'}></p>
+      <p className={'past-event__text_small'}></p>
+    </div>
   </div>
+  )
 }
