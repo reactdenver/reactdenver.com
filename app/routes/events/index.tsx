91969f9eb9cb8379f0252c1edd45b00d10360232
Pete Fowler <fowler.pete@gmail.com>
2022-10-22 14:11:33 -0600

    Remove past from class names

--- app/routes/events/index.tsx
+++ app/routes/events/index.tsx
@@ -24,17 +24,17 @@ export default function Events() {
     console.log(events);
   
   return (
-  <div className={'past-events'}>
-    <h4 className={'past-events__headline'}>Events:</h4>
+  <div className={'events'}>
+    <h4 className={'events__headline'}>Events:</h4>
     {eventsShown.map(event => 
-    <Link key={event.id} className={'past-event__content'} to={`./${event.slug}`}>
-      <img className={'past-event__image'} src={'https://source.unsplash.com/random/400x300'} alt={event.title}></img>
+    <Link key={event.id} className={'event__content'} to={`./${event.slug}`}>
+      <img className={'event__image'} src={'https://source.unsplash.com/random/400x300'} alt={event.title}></img>
       {/*=============== Image source to be changed from placeholder when available========= */}
-      <div className={'past-event__text-box'}>
-        <p className={'past-event__text_small'}>{typeof event.date === 'string' && format(new Date(event.date), 'EEE, MMM d, y')}</p>
-        <h3 className={'past-event__text_large'}>{event.title}</h3>
-        <p className={'past-event__text_small'}>{event.speakers?.map(speaker => `${speaker.name}`).join(', ')}</p>
-        <p className={'past-event__text_small'}>{event.location}</p>
+      <div className={'event__text-box'}>
+        <p className={'event__text_small'}>{typeof event.date === 'string' && format(new Date(event.date), 'EEE, MMM d, y')}</p>
+        <h3 className={'event__text_large'}>{event.title}</h3>
+        <p className={'event__text_small'}>{event.speakers?.map(speaker => `${speaker.name}`).join(', ')}</p>
+        <p className={'event__text_small'}>{event.location}</p>
       </div>
     </Link>)}
   </div>
