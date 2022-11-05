e10ca37de8de179ebbbdcfd33296a367b27c5608
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-11-05 16:06:22 -0600

    main page upcoming event data
    pulling next event data and rendering it on main page to replace static data.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,15 +5,19 @@ import Hero, { links as heroLinks } from "../components/hero";
 import Sponsors, { links as sponsorsLinks } from "../components/sponsors";
 import { getEventsJson } from "~/utils/events.server";
 import { json } from "@remix-run/node";
+import { format } from "date-fns";
 
 export const links = () => [...heroLinks(), ...sponsorsLinks()];
 
-const UpcomingEvent = ({ title, speakers, location }) => {
+const UpcomingEvent = ({ title, speakers, location, date }) => {
   return (
     <div>
       <div className="nextMeetupHero">
         <span>Next Meetup</span>
         <h2>{title}</h2>
+        <p className={"event__text_small"}>
+          {typeof date === "string" && format(new Date(date), "EEE, MMM d, y")}
+        </p>
         <p>{speakers}</p>
         <p>{location} & online</p>
       </div>
@@ -39,7 +43,12 @@ export default function Index() {
     (event) => event.date && new Date() >= new Date(event.date)
   );
 
-  //need to define variable for next event and use in info in JSX below
+  //get next event info
+  let eventNext = eventsAll.filter(
+    (event) => event.date && new Date() <= new Date(event.date)
+  );
+  eventNext.reverse();
+  eventNext.length > 1 ? (eventNext = eventNext.slice(-1)) : null;
 
   //only show most recent 4 events
   eventsPast.length > 4 ? (eventsPast = eventsPast.slice(-4)) : null;
@@ -53,9 +62,10 @@ export default function Index() {
         image={
           "https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80"
         }
-        title="Building React apps with End to End Encryption + Demystifying State Machines!"
-        speakers="Jane Goodall, Dennis Realman"
-        location="1595 Wynkoop Street, Denver CO"
+        date={eventNext[0]?.date}
+        title={eventNext[0]?.title}
+        speakers={eventNext[0]?.speakers[0]?.name}
+        location={eventNext[0]?.location}
       />
       <Sponsors />
       <span className="line"></span>
