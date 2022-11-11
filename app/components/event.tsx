5501ad5303d08dee2f4567803f6e59594cefbcdc
Jeff Baumgardt <medros1455@gmail.com>
2022-11-10 17:11:04 -0700

    Update events and event time issues. Better formatting. Yay

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { format } from "date-fns";
+import { format, addMinutes } from "date-fns";
 
 export default function Event(params: any, size: string) {
   const event = {
@@ -16,20 +16,24 @@ export default function Event(params: any, size: string) {
     ],
     front_image: params.event.front_image,
   };
-  console.log(params.event.front_image);
+
+  const actualDate = new Date(event.date);
 
   return (
-    <div className="event__content" style={{ pointerEvents: "none" }}>
+    <div className="event__content">
       <img
         className={`event__image_${params.size}`}
         src={event.front_image}
         alt={event.title}
-      ></img>
+      />
       {/*=============== Image source to be changed from placeholder when available========= */}
       <div className={"event__text-box"}>
         <p className={"event__text_small"}>
           {typeof event.date === "string" &&
-            format(new Date(event.date), "EEE, MMM d, y")}
+            format(
+              addMinutes(actualDate, actualDate.getTimezoneOffset()),
+              "EEE, MMM d, y"
+            )}
         </p>
         <h3 className={"event__text_large"}>{event.title}</h3>
         <p className={"event__text_small"}>
