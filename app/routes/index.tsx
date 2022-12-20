3b557bc2928babbb203454d1285f67d4553add7e
Pete Fowler <fowler.pete@gmail.com>
2022-12-19 19:42:07 -0700

    Add isSameDay day comparison method

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -50,9 +50,16 @@ export async function loader() {
 }
 
 export default function Index() {
+
+  function isSameDay(d1: Date, d2: Date) {
+    return d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate();
+  }
+
   const eventsAll = useLoaderData<typeof loader>();
   let eventsPast = eventsAll.filter(
-    (event) => event.date && new Date() >= new Date(event.date)
+    (event) => event.date && new Date() > new Date(event.date)
   );
 
   //get next event info
