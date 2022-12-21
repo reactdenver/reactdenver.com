99602ac0983d077bd759a1c332ec5d60e36b9864
Pete Fowler <fowler.pete@gmail.com>
2022-12-21 10:00:47 -0700

    Draft useEventDates refactor using Date.UTC

--- app/hooks/useEventDates.tsx
+++ app/hooks/useEventDates.tsx
@@ -1,26 +1,28 @@
 export default function useEventDates(events: any[]) {
-  const now: Date = new Date(new Date().setUTCHours(0, 0, 0, 0));
-
-  let eventsPast = events.filter(
-    (event) =>
-      event.date &&
-      now > new Date(event.date) &&
-      !isSameDay(now, new Date(event.date))
+  const now: Date = new Date();
+  const today: Date = new Date(
+    Date.UTC(now.getFullYear(), now.getMonth(), now.getDate())
   );
+  console.log(today == new Date(events[5].date));
+  let eventsPast = events.filter(
+    (event) => event.date && today > new Date(event.date)
+  );
+
+  // && !isSameDay(today, new Date(event.date))
 
   let eventNext = events.find(
-    (event) =>
-      event.date &&
-      (now <= new Date(event.date) || isSameDay(now, new Date(event.date)))
+    (event) => event.date && today <= new Date(event.date)
   );
 
-  function isSameDay(d1: Date, d2: Date): boolean {
-    return (
-      d1.getFullYear() === d2.getFullYear() &&
-      d1.getMonth() === d2.getMonth() &&
-      d1.getDate() === d2.getDate()
-    );
-  }
+  //  || isSameDay(today, new Date(event.date)))
+
+  // function isSameDay(d1: Date, d2: Date): boolean {
+  //   return (
+  //     d1.getUTCFullYear() === d2.getUTCFullYear() &&
+  //     d1.getUTCMonth() === d2.getUTCMonth() &&
+  //     d1.getUTCDate() === d2.getUTCDate()
+  //   );
+  // }
 
   return { eventNext, eventsPast };
 }
