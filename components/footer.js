99feaf31859310b3e0ef968e43de751d54e9a174
Jeff Baumgardt <medros1455@gmail.com>
2023-09-14 19:28:27 -0600

    Change out the footer with a link to netlify.

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,25 @@
 import Container from "@/components/container";
 import ThemeSwitch from "@/components/themeSwitch";
+import Image from "next/image";
+import netlifyImage from "@/components/netlify_icon-transparent.png";
 
 export default function Footer(props) {
   return (
     <Container className="mt-10 border-t border-gray-100 dark:border-gray-800">
       <div className="text-sm text-center">
-        Copyright © {new Date().getFullYear()} {props?.copyright}. All
-        rights reserved.
+        Copyright © {new Date().getFullYear()} {props?.copyright}. All rights
+        reserved.
       </div>
-      <div className="mt-1 text-sm text-center text-gray-500 dark:text-gray-600">
-        Made by{" "}
-        <a
-          href="https://www.web3templates.com/?ref=stablo-template"
-          rel="noopener"
-          target="_blank">
-          Web3Templates
+      <div className="mt-2 text-sm text-center text-gray-500 dark:text-gray-600">
+        <a href="https://www.netlify.com/" rel="noopener" target="_blank">
+          <Image
+            src={netlifyImage}
+            height={33}
+            width={33}
+            alt="netlify call to action"
+            className="inline relative -top-1"
+          />
+          Hosted by Netlify
         </a>
       </div>
       <div className="flex items-center justify-center mt-2">
