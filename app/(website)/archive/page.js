142a7c47c58a141778e4d41777e9a608417c6d79
Jeff Baumgardt <jeff.baumgardt@tempello.ai>
2024-02-16 12:47:34 -0700

    Weird suspense boundry error.

--- app/(website)/archive/page.js
+++ app/(website)/archive/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Archive from "./archive";
 
 import { getPaginatedEvents } from "@/lib/sanity/client";
@@ -6,7 +7,11 @@ const EVENTS_PER_PAGE = 6;
 
 export default async function ArchivePage() {
   const events = await getPaginatedEvents(EVENTS_PER_PAGE);
-  return <Archive events={events} />;
+  return (
+    <Suspense>
+      <Archive events={events} />
+    </Suspense>
+  );
 }
 
-// export const revalidate = 60;
+export const revalidate = 60;
